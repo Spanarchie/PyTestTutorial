d14f1020e1e3e02dc08d1862975593d0e74e6830
Colin Moore-Hill <spanarchiantesting@gmail.com>
2018-05-25 11:58:46 +0100

    Update test_nextwave.py

--- tests/test_nextwave.py
+++ tests/test_nextwave.py
@@ -1,28 +1,28 @@
 def test_init():
-    assert 1 == True
+    assert 1 == 1
 
 
 def test_init1():
-    assert 1 == True
+    assert 1 == 1
 
 
 def test_init2():
-    assert 1 == True
+    assert 1 == 1
 
 
 def test_init3():
-    assert 1 == True
+    assert 1 == 1
 
 
 def test_init4():
-    assert 1 == True
+    assert 1 == 1
 
 
 def test_init5():
-    assert 1 == True
+    assert 1 == 1
 
 
 def test_init6():
-    assert 1 == True
+    assert 1 == 1
 
 
